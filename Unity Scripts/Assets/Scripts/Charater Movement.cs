a764f197bbad71f41af4bc270c76febec9820597
eric <ericbrailsford8@gmail.com>
2018-09-10 10:10:47 -0600

    unity changes

--- Unity Scripts/Assets/Scripts/Charater Movement.cs
+++ Unity Scripts/Assets/Scripts/Charater Movement.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class CharaterMovement : MonoBehaviour {
 
+    // Player Movement Variables
+    public int MoveSpeed;
+    public float JumpHeight;
+
+    //Player grounded variables
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle.(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update () {
+    // This code makes ghe charater jump
+    if (Input.GetKeyDown)(KeyCode.Space)&& grounded) { Jump(); }
 	}
+}
+
+public void Jump()
+{
+    GetComponent<Rigidbody2D>().velocity = new Vector2(GetCompondent<Rigidbody2D>().velocity.x, JumpHeight);
+}
 }

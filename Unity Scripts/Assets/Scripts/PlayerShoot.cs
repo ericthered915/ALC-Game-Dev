2e02950e9a9d39afb9791419e788b704fdfe6454
Unknown <ericbrailsford8@gmail.com>
2018-10-22 10:04:22 -0600

    work day

--- Unity Scripts/Assets/Scripts/PlayerShoot.cs
+++ Unity Scripts/Assets/Scripts/PlayerShoot.cs
@@ -8,7 +8,7 @@ public class PlayerShoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.RightControl))
+		if (Input.GetKeyDown (KeyCode.LeftControl))
 			Instantiate (Projectile, FirePoint.position, FirePoint.rotation);
 		
 	}

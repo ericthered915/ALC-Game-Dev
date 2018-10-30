03ec59226b113700ee0c07393ac5f1038959f81b
Unknown <ericbrailsford8@gmail.com>
2018-10-30 10:29:47 -0600

    camera follow script

---
+++ Unity Scripts/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public CharacterMove Player;
+    public bool isFollowing;
+
+    // Camera postition offset
+    public float xOffset;
+    public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+        Player = FindObjectOfType<CharacterMove>();
+
+        isFollowing = true;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isFollowing){
+            transform.position = new Vector3(Player.transform.position.x + xOffset, Player.transform.position.y + yOffset, transform.position.z);
+        }
+		
+	}
+}

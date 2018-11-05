8c0d9971fe4acb7b5064a64158a156debf93fd9b
Unknown <ericbrailsford8@gmail.com>
2018-11-05 10:09:50 -0700

    Graphics

--- Unity Scripts/Assets/Scripts/CharacterMove.cs
+++ Unity Scripts/Assets/Scripts/CharacterMove.cs
@@ -68,9 +68,9 @@ public class CharacterMove: MonoBehaviour
 
 			//Player flip
 			if (GetComponent<Rigidbody2D> ().velocity.x > 0)
-				transform.localScale = new Vector3 (38f, 30f, 9f);
+				transform.localScale = new Vector3 (7f, 6f, 1f);
 			else if (GetComponent<Rigidbody2D> ().velocity.x < 0)
-				transform.localScale = new Vector3 (-38f,-30f,9f);
+				transform.localScale = new Vector3 (-7f,-6f,1f);
 		
 	}
 	public void Jump(){

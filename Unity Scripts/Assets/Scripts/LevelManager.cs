cc97c4426a8566b5aa4a6940fa47d5751c6bcc05
Unknown <ericbrailsford8@gmail.com>
2018-12-06 10:27:36 -0700

    hm

--- Unity Scripts/Assets/Scripts/LevelManager.cs
+++ Unity Scripts/Assets/Scripts/LevelManager.cs
@@ -1,74 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class LevelManager : MonoBehaviour {
-	public GameObject CurrentCheckPoint;
-	public Rigidbody2D Player;
+public class LevelManager : MonoBehaviour
+{
+
+    public GameObject CurrentCheckPoint;
+    public Rigidbody2D Player;
+
     public GameObject Player2;
 
-	//Particles
-	public GameObject deathParticle;
-	public GameObject RespawnParticle;
+    // Particles
+    public GameObject DeathParticle;
+    public GameObject RespawnParticle;
+
+    //Respawn Delay
+    public float RespawnDelay;
 
 
-	//Respawn Delay
-	public float respawnDelay;
+    //Point Penalty on Death
+    public int PointPenaltyOnDeath;
 
-	//Point Penalty on Death
-	public int PointPenaltyOnDeath;
+    // Store Gravity Value
+    private float GravityStore;
 
-	// Store Gravity Value
-	private float gravityStore;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         Player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
         Player2 = GameObject.Find("Player");
-	}
+    }
 
-	public void RespawnPlayer(){
-		StartCoroutine ("RespawnPlayerCo");
-	}
+    public void RespawnPlayer()
+    {
+        StartCoroutine("RespawnPlayerCo");
+    }
 
-	public IEnumerator RespawnPlayerCo(){
-		//Generate Death Particle
-		Instantiate (deathParticle, Player.transform.position, Player.transform.rotation);
-        //HidePlayer
-        // Player.enabled = false;
+    public IEnumerator RespawnPlayerCo()
+    {
+        //Generate Death Particle
+        Instantiate(DeathParticle, Player.transform.position, Player.transform.rotation);
+        //Hide PC
+        // PC.enabled = false;
         Player2.SetActive(false);
-		Player.GetComponent<Renderer> ().enabled = false;
-		// Gravity Reset
-		gravityStore = Player.GetComponent<Rigidbody2D> ().gravityScale;
-		Player.GetComponent<Rigidbody2D> ().gravityScale = 0f;
-		Player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
-		// Point Penalty
-		ScoreManager.AddPoints (-PointPenaltyOnDeath);
-		//Debug Message
-		Debug.Log ("Player Respawn");
-		//Respawn Delay
-		yield return new WaitForSeconds (respawnDelay);
-		//Gravity Restore
-		Player.GetComponent<Rigidbody2D> ().gravityScale = gravityStore;
-		//Match Players transform position
-		Player.transform.position = CurrentCheckPoint.transform.position;
-		//ShowPlayer
-		//Player.enabled = true;
-		Player.GetComponent<Renderer> ().enabled = true;
+        Player.GetComponent<Renderer>().enabled = false;
+        // Gravity Reset
+        GravityStore = Player.GetComponent<Rigidbody2D>().gravityScale;
+        Player.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        Player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        // Point Penalty
+        ScoreManager.AddPoints(-PointPenaltyOnDeath);
+        //Debug Message
+        Debug.Log("Player Respawn");
+        //Respawn Delay
+        yield return new WaitForSeconds(RespawnDelay);
+        //Gravity Restore
+        Player.GetComponent<Rigidbody2D>().gravityScale = GravityStore;
+        //Match Players transform position
+        Player.transform.position = CurrentCheckPoint.transform.position;
+        //Show Player
+        // Player.enabled = true;
         Player2.SetActive(true);
-
-
-		//Spawn Player
-		Instantiate (RespawnParticle, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
-	
-
-
-
-
-		
-	}
-	
-
-		
-	}
-
+        Player.GetComponent<Renderer>().enabled = true;
+        //Spawn PC
+        Instantiate(RespawnParticle, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
+    }
+}

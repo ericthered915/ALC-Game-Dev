677ae7383e1b9b6ce6142231f229ed85c6c475ab
Unknown <ericbrailsford8@gmail.com>
2019-01-08 10:29:08 -0700

    yote

--- Unity Scripts/Assets/Scripts/CharacterMove.cs
+++ Unity Scripts/Assets/Scripts/CharacterMove.cs
@@ -4,17 +4,17 @@ using System.Collections;
 public class CharacterMove : MonoBehaviour
 {
 
-    // Player Movement Variables
-    public int MoveSpeed;
+   // Player Movement Variables
+   public int MoveSpeed;
     public float JumpHeight;
 
-    //Player grounded variables
+   //Player grounded variables
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool grounded;
 
-    // Use this for initialization
+   // Use this for initialization
     void Start()
     {
 
@@ -22,31 +22,29 @@ public class CharacterMove : MonoBehaviour
 
 
     void FixedUpdate()
-    {
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    {grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        // This code makes the character jump
+       // This code makes the character jump
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             Jump();
         }
 
-        // This code makes the character move from side to side using the A&D keys
+       // This code makes the character move from side to side using the A&D keys
         if (Input.GetKey(KeyCode.D))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        {GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
         }
         if (Input.GetKey(KeyCode.A))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+          GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
-        }
+     }
 
     }
 
